0c352eaa5a0381fdc7f286645734e632ec3de215
david <github@nonswag.dev>
2025-04-21 15:53:46 +0200

    Removed unused setup and cleanup methods in BaseTest
    Simplified `BaseTest` by eliminating unnecessary annotations, methods, and redundant file handling logic.
    
    Signed-off-by: david <github@nonswag.dev>

--- src/test/java/org/faststats/BaseTest.java
+++ src/test/java/org/faststats/BaseTest.java
@@ -2,35 +2,18 @@ package org.faststats;
 
 import org.faststats.controller.DatabaseController;
 import org.jspecify.annotations.NullMarked;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 
-import java.io.File;
 import java.sql.SQLException;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @NullMarked
 public class BaseTest {
-    private static final File databaseFile = new File("test.db");
     private final String url = "jdbc:postgresql://postgres:5432/data?user=postgres&password=postgres";
     protected DatabaseController database = new DatabaseController(url);
-
-    @AfterAll
-    @BeforeAll
-    public static void cleanup() {
-        deleteDatabase();
-    }
 
     @AfterEach
     public void cleanupDatabase() throws SQLException {
         database.close();
-        deleteDatabase();
         database = new DatabaseController(url);
-    }
-
-    private static void deleteDatabase() {
-        assertTrue(!databaseFile.exists() | databaseFile.delete(), "Failed to delete database file");
     }
 }
